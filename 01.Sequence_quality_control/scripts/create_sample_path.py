54dc16496122b568700cd9a17d86039f7b706f90
Zewei Song <songzewei@outlook.com>
2024-06-21 09:18:38 +0800

    Update create_sample_path.py

--- 01.Sequence_quality_control/scripts/create_sample_path.py
+++ 01.Sequence_quality_control/scripts/create_sample_path.py
@@ -1,19 +1,20 @@
 import os
 content = {}
-with open('/dssg/home/acct-trench/trench/DATA/MEER_RAW/metadata.tsv', 'r') as f:
+with open('metadata.tsv', 'r') as f:
     for line in f:
         line = line.strip('\n').split('\t')
         content[line[0]] = content.get(line[0], []) + [line[1]]
 
 samples = {}
-with open('tst_low_depth/data/list', 'r') as f:
+# list is a text file contains a list of sample names to be included
+with open('data/list', 'r') as f:
     for line in f:
         line = line.strip('\n').split('\t')
         samples[line[0]] = content[line[0]]
 
 # write to .path fike
 for key, value in samples.items():
-    with open('tst_low_depth/data/samples/' + key + '.path', 'w') as f:
+    with open('data/samples/' + key + '.path', 'w') as f:
         for line in value:
             f.write('{0}\n'.format(line))
     print('{0} created'.format(key))
